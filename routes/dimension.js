954f71cc8fe099bd6f463eef67607dce4280a0e8
Anupam Singh Sikarwar <sikarwar1606@gmail.com>
2025-10-26 10:34:37 +0530

    Added condition while loading the existing data

--- routes/dimension.js
+++ routes/dimension.js
@@ -22,7 +22,7 @@ function getShiftDate() {
   const month = String(now.getMonth() + 1).padStart(2, '0');
   const year = now.getFullYear();
 
-  return `${day}/${month}/${year}`;
+  return `${day}-${month}-${year}`;
 }
 let inspection;
 
